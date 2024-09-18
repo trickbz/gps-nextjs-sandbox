4a1ad3f50604cef71c21e9d34286459ea236c614
apetrenko <trickbz@gmail.com>
2024-09-18 03:56:44 +0300

    replaced generated ids fix hardcoded in mock data

--- src/mocks/bands.mock.ts
+++ src/mocks/bands.mock.ts
@@ -1,16 +1,14 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { Band } from '@/types/band.types';
 
 export const BANDS_MOCK: Band[] = [
   {
-    id: uuidv4(),
+    id: 'band-1',
     name: 'The Beatles',
     description: 'An iconic British rock band formed in Liverpool in 1960.',
     image: '/images/beatles.png',
     members: [
       {
-        id: uuidv4(),
+        id: 'member-1-1',
         firstName: 'John',
         lastName: 'Lennon',
         instruments: ['Guitar', 'Vocals'],
@@ -19,7 +17,7 @@ export const BANDS_MOCK: Band[] = [
           "John Lennon was a founding member of The Beatles and a key figure in the band's success. He was known for his distinctive voice and songwriting skills.",
       },
       {
-        id: uuidv4(),
+        id: 'member-1-2',
         firstName: 'Paul',
         lastName: 'McCartney',
         instruments: ['Bass', 'Vocals'],
@@ -28,7 +26,7 @@ export const BANDS_MOCK: Band[] = [
           'Paul McCartney is a legendary musician and songwriter, best known as the bassist and co-lead vocalist for The Beatles.',
       },
       {
-        id: uuidv4(),
+        id: 'member-1-3',
         firstName: 'George',
         lastName: 'Harrison',
         instruments: ['Guitar', 'Vocals'],
@@ -37,7 +35,7 @@ export const BANDS_MOCK: Band[] = [
           'George Harrison was the lead guitarist of The Beatles, known for his melodic playing and incorporation of Indian music influences.',
       },
       {
-        id: uuidv4(),
+        id: 'member-1-4',
         firstName: 'Ringo',
         lastName: 'Starr',
         instruments: ['Drums', 'Vocals'],
@@ -48,13 +46,13 @@ export const BANDS_MOCK: Band[] = [
     ],
     albums: [
       {
-        id: uuidv4(),
+        id: 'album-1-1',
         title: 'Abbey Road',
         releaseDate: '1969-09-26',
         image: 'https://example.com/abbeyroad.jpg',
       },
       {
-        id: uuidv4(),
+        id: 'album-1-2',
         title: "Sgt. Pepper's Lonely Hearts Club Band",
         releaseDate: '1967-05-26',
         image: 'https://example.com/sgtpepper.jpg',
@@ -62,13 +60,13 @@ export const BANDS_MOCK: Band[] = [
     ],
   },
   {
-    id: uuidv4(),
+    id: 'band-2',
     name: 'Queen',
     description: 'A British rock band formed in London in 1970.',
     image: '/images/beatles.png',
     members: [
       {
-        id: uuidv4(),
+        id: 'member-2-1',
         firstName: 'Freddie',
         lastName: 'Mercury',
         instruments: ['Vocals', 'Keyboard'],
@@ -77,7 +75,7 @@ export const BANDS_MOCK: Band[] = [
           'Freddie Mercury was the charismatic lead vocalist of Queen, known for his powerful voice and dynamic stage presence.',
       },
       {
-        id: uuidv4(),
+        id: 'member-2-2',
         firstName: 'Brian',
         lastName: 'May',
         instruments: ['Guitar', 'Vocals'],
@@ -86,7 +84,7 @@ export const BANDS_MOCK: Band[] = [
           'Brian May is the lead guitarist of Queen, recognized for his distinctive guitar sound and technical prowess.',
       },
       {
-        id: uuidv4(),
+        id: 'member-2-3',
         firstName: 'Roger',
         lastName: 'Taylor',
         instruments: ['Drums', 'Vocals'],
@@ -95,7 +93,7 @@ export const BANDS_MOCK: Band[] = [
           'Roger Taylor is the drummer for Queen, known for his energetic drumming and vocal contributions.',
       },
       {
-        id: uuidv4(),
+        id: 'member-2-4',
         firstName: 'John',
         lastName: 'Deacon',
         instruments: ['Bass'],
@@ -106,13 +104,13 @@ export const BANDS_MOCK: Band[] = [
     ],
     albums: [
       {
-        id: uuidv4(),
+        id: 'album-2-1',
         title: 'A Night at the Opera',
         releaseDate: '1975-11-21',
         image: 'https://example.com/anightattheopera.jpg',
       },
       {
-        id: uuidv4(),
+        id: 'album-2-2',
         title: 'The Game',
         releaseDate: '1980-06-30',
         image: 'https://example.com/thegame.jpg',
@@ -120,14 +118,14 @@ export const BANDS_MOCK: Band[] = [
     ],
   },
   {
-    id: uuidv4(),
+    id: 'band-3',
     name: 'Nirvana',
     description:
       'An American rock band formed in Aberdeen, Washington, in 1987.',
     image: '/images/beatles.png',
     members: [
       {
-        id: uuidv4(),
+        id: 'member-3-1',
         firstName: 'Kurt',
         lastName: 'Cobain',
         instruments: ['Guitar', 'Vocals'],
@@ -136,7 +134,7 @@ export const BANDS_MOCK: Band[] = [
           'Kurt Cobain was the lead vocalist and guitarist of Nirvana, known for his raw voice and influential songwriting.',
       },
       {
-        id: uuidv4(),
+        id: 'member-3-2',
         firstName: 'Krist',
         lastName: 'Novoselic',
         instruments: ['Bass'],
@@ -145,7 +143,7 @@ export const BANDS_MOCK: Band[] = [
           'Krist Novoselic was the bassist for Nirvana, known for his powerful bass lines and stage presence.',
       },
       {
-        id: uuidv4(),
+        id: 'member-3-3',
         firstName: 'Dave',
         lastName: 'Grohl',
         instruments: ['Drums'],
@@ -156,13 +154,13 @@ export const BANDS_MOCK: Band[] = [
     ],
     albums: [
       {
-        id: uuidv4(),
+        id: 'album-3-1',
         title: 'Nevermind',
         releaseDate: '1991-09-24',
         image: 'https://example.com/nevermind.jpg',
       },
       {
-        id: uuidv4(),
+        id: 'album-3-2',
         title: 'In Utero',
         releaseDate: '1993-09-21',
         image: 'https://example.com/inutero.jpg',
